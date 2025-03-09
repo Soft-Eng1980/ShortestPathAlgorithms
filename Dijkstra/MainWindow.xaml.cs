718ade4c84c616a3ec668d02816a640f39065f3c
Eng-Dev1980 <Hayyadar@googlemail.com>
2025-03-09 08:23:13 +0330

    1: Code behind refactored 2: Fake data generator added 3: Points Textbox data binfdng added

--- Dijkstra/MainWindow.xaml.cs
+++ Dijkstra/MainWindow.xaml.cs
@@ -15,43 +15,21 @@ using System.Windows.Shapes;
 
 namespace Dijkstra
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        //----------------------------------------------------------------------------------------------------------------------------\\
         public MainWindow()
         {
             InitializeComponent();
         }
-        //----------------------------------------------------------------------------------------------------------------------------\\
-        private void btn_Lecturer_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("DR.M.Yadollah Zadeh Tabari");
-        }
-        //----------------------------------------------------------------------------------------------------------------------------\\
-        private void btn_Creators_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Mahdi JafariPour && Mohammad Baqer Haeri Bazzaz");
-        }
-        //----------------------------------------------------------------------------------------------------------------------------\\
-        private void btn_Course_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Algorithms Design");
-        }
-        //----------------------------------------------------------------------------------------------------------------------------\\
-        private void btn_calc_Click(object sender, RoutedEventArgs e)
-        {
 
-        }
-        //----------------------------------------------------------------------------------------------------------------------------\\
+        #region Initialization
+
         private void Load(object sender, RoutedEventArgs e)
         {
             foreach (TextBox item in GUIGrid.Children.OfType<TextBox>())
             {
                 string[] name = item.Name.Split("_");
-                PanelStackPanel.Children.Add(new RoundedTextBox()
+                RoundedTextBox newChild = new()
                 {
                     Name = $"{item.Name}_Panel",
                     Margin = new Thickness(5),
@@ -60,34 +38,75 @@ namespace Dijkstra
                     BorderThickness = new Thickness(1),
                     BorderBrush = new SolidColorBrush(Colors.Black),
                     TextBoxsTextAlignment = TextAlignment.Center,
-                    TextBlocksTextAlignment  = TextAlignment.Center,
+                    TextBlocksTextAlignment = TextAlignment.Center,
                     IsNumeric = true,
                     MaxLines = 1,
                     TextMaxLenght = 3,
                     MinValue = 1,
 
+                };
+
+                newChild.SetBinding(RoundedTextBox.TextProperty, new Binding("Text")
+                {
+                    Source = item,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
                 });
+
+                item.SetBinding(TextBox.TextProperty, new Binding("Text")
+                {
+                    Source = newChild.baseTextBox,
+                    Mode = BindingMode.TwoWay,
+                    UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                });
+
+                PanelStackPanel.Children.Add(newChild);
             }
 
-            Inception_Point.Items.Add("A");
-            Inception_Point.Items.Add("B");
-            Inception_Point.Items.Add("C");
-            Inception_Point.Items.Add("D");
-            Inception_Point.Items.Add("E");
-            Inception_Point.Items.Add("F");
-            Inception_Point.Items.Add("G");
-            Inception_Point.Items.Add("H");
-            Inception_Point.Items.Add("I");
-            Inception_Point.Items.Add("J");
-            Inception_Point.Items.Add("K");
-            Inception_Point.Items.Add("L");
-            Inception_Point.Items.Add("M");
-            Inception_Point.Items.Add("N");
-            Inception_Point.Items.Add("O");
-            Inception_Point.Items.Add("P");
-            Inception_Point.Items.Add("Q");
+            Inception_Point.ItemsSource = Destination_Point.ItemsSource = new List<string>(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q"]);
+
+
+
+        }
+        
+        #endregion
+
+        #region Operations
+
+        private void OnCalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //----------------------------------------------------------------------------------------------------------------------------\\
+
+        private void OnFakeDataButtonClick(object sender, RoutedEventArgs e)
+        {
+            Random random = new();
+            foreach (RoundedTextBox item in PanelStackPanel.Children.OfType<RoundedTextBox>())
+            {
+                item.Text = random.Next(1, 999).ToString();
+            }
+        }
+        #endregion
+
+        #region Credits
+
+        private void OnLecturerButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("DR.M.Yadollah Zadeh Tabari");
         }
         //----------------------------------------------------------------------------------------------------------------------------\\
+        private void OnCreatorsButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Mohammad Baqer Haeri Bazzaz");
+        }
+        //----------------------------------------------------------------------------------------------------------------------------\\
+        private void OnCourseButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Algorithms Design");
+        }
+
+        #endregion
     }
-    //----------------------------------------------------------------------------------------------------------------------------\\
 }
